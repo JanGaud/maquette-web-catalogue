9c4d09355139f50d8301dcf6abdbd412baa68b88
JanGaud <JanisGaudreault.jg@gmail.com>
2022-11-22 09:08:58 -0500

    Page de fiche recommencé... la loupe sur l'image fonctionne

--- js/loupe.js
+++ js/loupe.js
@@ -1,65 +1,58 @@
-// https://www.w3schools.com/howto/howto_js_image_magnifier_glass.asp
-
 function magnify(imgID, zoom) {
     var img, glass, w, h, bw;
     img = document.getElementById(imgID);
-  
-    /* Create magnifier glass: */
+    /*create magnifier glass:*/
     glass = document.createElement("DIV");
     glass.setAttribute("class", "img-magnifier-glass");
-  
-    /* Insert magnifier glass: */
+    /*insert magnifier glass:*/
     img.parentElement.insertBefore(glass, img);
-  
-    /* Set background properties for the magnifier glass: */
+    /*set background properties for the magnifier glass:*/
     glass.style.backgroundImage = "url('" + img.src + "')";
     glass.style.backgroundRepeat = "no-repeat";
     glass.style.backgroundSize = (img.width * zoom) + "px " + (img.height * zoom) + "px";
     bw = 3;
     w = glass.offsetWidth / 2;
     h = glass.offsetHeight / 2;
-  
-    /* Execute a function when someone moves the magnifier glass over the image: */
+    /*execute a function when someone moves the magnifier glass over the image:*/
     glass.addEventListener("mousemove", moveMagnifier);
     img.addEventListener("mousemove", moveMagnifier);
-  
     /*and also for touch screens:*/
     glass.addEventListener("touchmove", moveMagnifier);
     img.addEventListener("touchmove", moveMagnifier);
     function moveMagnifier(e) {
       var pos, x, y;
-      /* Prevent any other actions that may occur when moving over the image */
+      /*prevent any other actions that may occur when moving over the image*/
       e.preventDefault();
-      /* Get the cursor's x and y positions: */
+      /*get the cursor's x and y positions:*/
       pos = getCursorPos(e);
       x = pos.x;
       y = pos.y;
-      /* Prevent the magnifier glass from being positioned outside the image: */
+      /*prevent the magnifier glass from being positioned outside the image:*/
       if (x > img.width - (w / zoom)) {x = img.width - (w / zoom);}
       if (x < w / zoom) {x = w / zoom;}
       if (y > img.height - (h / zoom)) {y = img.height - (h / zoom);}
       if (y < h / zoom) {y = h / zoom;}
-      /* Set the position of the magnifier glass: */
+      /*set the position of the magnifier glass:*/
       glass.style.left = (x - w) + "px";
       glass.style.top = (y - h) + "px";
-      /* Display what the magnifier glass "sees": */
+      /*display what the magnifier glass "sees":*/
       glass.style.backgroundPosition = "-" + ((x * zoom) - w + bw) + "px -" + ((y * zoom) - h + bw) + "px";
     }
-  
     function getCursorPos(e) {
       var a, x = 0, y = 0;
       e = e || window.event;
-      /* Get the x and y positions of the image: */
+      /*get the x and y positions of the image:*/
       a = img.getBoundingClientRect();
-      /* Calculate the cursor's x and y coordinates, relative to the image: */
+      /*calculate the cursor's x and y coordinates, relative to the image:*/
       x = e.pageX - a.left;
       y = e.pageY - a.top;
-      /* Consider any page scrolling: */
+      /*consider any page scrolling:*/
       x = x - window.pageXOffset;
       y = y - window.pageYOffset;
       return {x : x, y : y};
     }
   }
 
+  magnify("big-img", 2);
 
-  magnify("loupeImg", 3);
+ /*https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_image_magnifier_glass*/
